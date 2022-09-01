40518fca9942bd8666bda7e7fa29621649629472
Pedro <pedrobiqua@gmail.com>
2022-08-31 21:45:34 -0300

    fix #3

--- Fabricante.java
+++ Fabricante.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class Fabricante extends Thread{
     char nomeFabricante;
-    private Semaphore mutex, itens, itens2, mutexEntregas;
+    private Semaphore mutexVendas, itens, itens2, mutexEntregas;
     FilaVenda vendas;
     FilaEntrega entregas;
 
 
-    public Fabricante(FilaVenda vendas, FilaEntrega entregas, Semaphore mutex, Semaphore itens, Semaphore itens2, Semaphore mutexEntregas){
+    public Fabricante(FilaVenda vendas, FilaEntrega entregas, Semaphore mutexVendas, Semaphore itens, Semaphore itens2, Semaphore mutexEntregas){
         this.vendas = vendas;
         this.entregas = entregas;
-        this.mutex = mutex;
+        this.mutexVendas = mutexVendas;
         this.itens = itens;
         this.itens2 = itens2;
         this.mutexEntregas = mutexEntregas;
@@ -27,19 +27,18 @@ public class Fabricante extends Thread{
                 
                 itens.acquire();
                     
-                    mutex.acquire();
+                    mutexVendas.acquire();
                         Venda newVenda = vendas.vendas.get(0);
                         vendas.vendas.remove(vendas.vendas.get(0));
-                    mutex.release();
+                    mutexVendas.release();
 
                     Thread.sleep(random.nextInt(1000)); //arrumar intervalo conforme a tabela no .pdf
+                    
                     // Cria entrega
-
                     Entrega entrega = new Entrega();
                     entrega.numeroEntrega++;
                     entrega.venda = newVenda;
                     
-                    //criar segundo mutex
                     mutexEntregas.acquire();
                         entregas.entregas.add(entrega);
                         System.out.println("Fila de entregas: " + entregas.entregas.size());
@@ -50,7 +49,6 @@ public class Fabricante extends Thread{
                     System.out.println("Fabricante retira produto");
                 
                 itens2.release();
-                //Criar outro itens.release();
             }
             catch (InterruptedException e) {
 				e.printStackTrace();
